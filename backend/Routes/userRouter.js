2379b454c91542bbef8ef456a6d3463675357299
Ankit singh <singhankit0460@gmail.com>
2024-08-01 01:41:04 +0530

    backend

--- backend/Routes/userRouter.js
+++ backend/Routes/userRouter.js
@@ -1,5 +1,17 @@
 import express from "express";
+import {
+  getUser,
+  logout,
+  register,
+  signIn,
+} from "../controllers/userController.js";
+import { isAuthorised } from "../middlewares/auth.js";
 
-const router = express.Router;
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/signin", signIn);
+router.get("/logout", isAuthorised, logout);
+router.get("/getuser", isAuthorised, getUser);
 
 export default router;
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-import { catchAsyncError } from "./catchAsyncError";
+import { catchAsyncError } from "./catchAsyncError.js";
 import { User } from "../models/userModel.js";
-
+import ErrorHandler from "./error.js";
 export const isAuthorised = catchAsyncError(async (req, res, next) => {
   const { token } = req.cookies;
   if (!token) {
-    return next(new ErrorHandler("USer is not authorized", 400));
+    return next(new ErrorHandler("User is not authorized", 400));
   }
   const decode = jwt.verify(token, process.env.SECRET);
   req.user = await User.findById(decode.id);
