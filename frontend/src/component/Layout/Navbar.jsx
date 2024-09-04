e9d03cb4577e56af946d01f420e579156953213c
Ankit singh <singhankit0460@gmail.com>
2024-09-04 13:50:15 +0530

    navbar

--- frontend/src/component/Layout/Navbar.jsx
+++ frontend/src/component/Layout/Navbar.jsx
@@ -5,7 +5,57 @@ function Navbar() {
   const [show, setShow] = useState(false);
   const [isAuthorized, setIsAuthorised, user] = useContext(Context);
 
-  return <></>;
+  return (
+    <>
+      <nav className={isAuthorized ? "navbarShow" : "navbarHide"}>
+        <div className="container">
+          <div className="logo">
+            <img src="/JobZee-logos__white.png" alt="logo" />
+          </div>
+          <ul className={!show ? "menu" : "show-menu menu"}>
+            <li>
+              <Link to={"/"} onClick={() => setShow(false)}>
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link to={"/job/getall"} onClick={() => setShow(false)}>
+                ALL JOBS
+              </Link>
+            </li>
+            <li>
+              <Link to={"/applications/me"} onClick={() => setShow(false)}>
+                {user && user.role === "Employer"
+                  ? "APPLICANT'S APPLICATIONS"
+                  : "MY APPLICATIONS"}
+              </Link>
+            </li>
+            {user && user.role === "Employer" ? (
+              <>
+                <li>
+                  <Link to={"/job/post"} onClick={() => setShow(false)}>
+                    POST NEW JOB
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"/job/me"} onClick={() => setShow(false)}>
+                    VIEW YOUR JOBS
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <></>
+            )}
+
+            <button onClick={handleLogout}>LOGOUT</button>
+          </ul>
+          <div className="hamburger">
+            <GiHamburgerMenu onClick={() => setShow(!show)} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
